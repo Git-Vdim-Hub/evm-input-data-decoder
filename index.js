1cbd7b37c62221d591a6c3ddd5c0c95321e57e31
Git-Vdim-Hub <vadimrace@gmail.com>
2023-08-04 17:38:38 -0400

    added shell code

---
+++ index.js
@@ -0,0 +1,14 @@
+function decodeInput(input) {
+    return "Decoded Input comes out here"
+}
+
+function consoleLogDecodedInput() {
+    console.log(decodeInput())
+}
+
+function getContractAddress(decodedInput, walletAddress){
+    console.log("Pikachu")
+    return "contract address will be output from here"
+}
+
+consoleLogDecodedInput()
