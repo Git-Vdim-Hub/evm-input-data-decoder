e9194d6ff55af54ed3d5271fea16f8f2a2f85a2b
NowrinIslamMim <nowrin02@gmail.com>
2023-08-15 15:51:02 +0600

    comment added on code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Web3 = require('web3');
 
-const input = "0x7ff36ab500000000000000000000000000000000000000000000048fd9b015d231b091770000000000000000000000000000000000000000000000000000000000000080000000000000000000000000977223ef93b8490e8e6d2dc28567360f489a3ee10000000000000000000000000000000000000000000000000000000061b2b7e40000000000000000000000000000000000000000000000000000000000000002000000000000000000000000c02aaa39b223fe8d0a0e5c4f27ead9083c756cc200000000000000000000000056a86d648c435dc707c8405b78e2ae8eb4e60ba4" // Example input for a "transfer" function
+const input = "0x38ed17390000000000000000000000000000000000000000000000000000000165a0bc00000000000000000000000000000000000000000000000004dbab6733677e9cf800000000000000000000000000000000000000000000000000000000000000a0000000000000000000000000977223ef93b8490e8e6d2dc28567360f489a3ee100000000000000000000000000000000000000000000000000000000604913f70000000000000000000000000000000000000000000000000000000000000003000000000000000000000000dac17f958d2ee523a2206206994597c13d831ec7000000000000000000000000c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2000000000000000000000000990f341946a3fdb507ae7e52d17851b87168017c" // Example input for a "transfer" function
 
 consoleLogDecodedInput();
 
@@ -27,13 +27,13 @@ function consoleLogDecodedInput() {
 function getContractAddress(input_data) {
     let tokenAddress = null;
 
-    // Check for `swapETHForExactTokens`
+    // Check for swapETHForExactTokens(uint256 amountOut, address[] path, address to, uint256 deadline)
     if (input_data.startsWith('0xfb3bdb41')) {
         const tokenAddress = '0x' + input_data.slice(-40);
         return tokenAddress;
     }
 
-    // Handle swapTokensForExactTokens
+    // Check for swapTokensForExactTokens(uint256 amountOut, uint256 amountInMax, address[] path, address to, uint256 deadline)
     if (input_data.startsWith("0x8803dbee")) {
         const params = Web3.eth.abi.decodeParameters(
             [
@@ -58,7 +58,7 @@ function getContractAddress(input_data) {
         }
     }
 
-    // Check for method ID matching swapExactETHForTokens
+    // Check for swapExactETHForTokens(uint256 amountOutMin, address[] path, address to, uint256 deadline)
     if (input_data.startsWith("0x7ff36ab5")) {
         const params = Web3.eth.abi.decodeParameters(
             [
@@ -67,7 +67,7 @@ function getContractAddress(input_data) {
                 'address',
                 'uint256'
             ],
-            '0x' + input_data.slice(10) // Remove the method ID
+            '0x' + input_data.slice(10)
         );
 
         if (Array.isArray(params['1']) && params['1'].length > 0) {
@@ -76,6 +76,27 @@ function getContractAddress(input_data) {
         } else {
             console.log("Unable to find the token contract address in the given input data");
         }
+    }
+
+
+    //swapExactTokensForTokens(uint256 amountIn, uint256 amountOutMin, address[] path, address to, uint256 deadline)
+
+    if (input_data.startsWith("0x38ed1739")) {
+
+        const params = Web3.eth.abi.decodeParameters(
+            [
+                'uint256',
+                'uint256',
+                'address[]',
+                'address',
+                'uint256'
+            ],
+            '0x' + input_data.slice(10)
+        );
+
+        // params will now contain the values for amountIn, amountOutMin, path, to, and deadline
+        const tokenAddress = params[2][params[2].length - 1]; // Last address in path may be the token contract address
+        return tokenAddress;
     }
 
     // Check for `transfer(address _to, uint256 _value)`
@@ -89,9 +110,6 @@ function getContractAddress(input_data) {
         const tokenAddress = '0x' + input_data.slice(34, 74);
         return tokenAddress;
     }
-
-
-
 
     // If you have additional function signatures, add them here...
 
